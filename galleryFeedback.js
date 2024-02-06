261d5cdba75baf07c605505050dd180f5ec0738f
rosewinston <124084922+rosewinston@users.noreply.github.com>
2024-02-06 15:25:45 -0500

    WIP for updating for exhibition functionality

--- galleryFeedback.js
+++ galleryFeedback.js
@@ -1,4 +1,4 @@
-var baseUrl = 'http://54.81.146.159:5005';
+var baseUrl = 'http://138.28.162.217:5005';
 var state="off";
 var myname="";
 var art_piece="";
@@ -10,27 +10,97 @@ var username = "";
 var password = "";
 var word = ""; 
 
-document.getElementById('submit-btn').addEventListener("click", getWord);
+	// document.getElementById('submit-btn').addEventListener("click", getWord); unsure if used for new code
 
-function getWord() {
-   word = document.getElementById('word-response').value;
-   fetch(baseUrl + '/response/word/'+word, {
-    method: 'get'
-   })
-   .then(response => response.json())
-   .then(data => completeGetWord(data))
-   .catch(error => {
-    {alert("Error: Something went wrong:" + error); }
-   })
+function siteDropdown(exhibitionList, exhibitionLink) { 
+	var dropdown = "";
+	var countValue = 0;
+	artList.forEach(function(key){
+		dropdown += "<option value='"+exhibitionLink[countValue]+"'>"+key+"</option>";
+		countValue += 1;
+	})
+	
+	var imageList = "";
+	var count2 = 0;
+	artLink.forEach(function(key){
+		imageList += "<img id='"+key+"' src='"+gundUrl+key+"' alt='img' draggable='false'>";
+	})
+
+	document.querySelector('#slct').innerHTML = dropdown;
+	document.querySelector('.slider').innerHTML = imageList;
+	return;
 }
 
 
-function completeGetWord(results){
-	var word = results['word'];
-	console.log("Word: "+word);
-	clearTextbox();
-	alert("Success! Word added");
+function getAllExhibitions(){
+	fetch(baseUrl+'/response/getAllExhibitions', {
+		method: 'get'	
+	})
+	.then (response => response.json())
+    .then (data =>completeGetExhibitions(data))
+    .catch(error => {
+        {alert("Error: Something went wrong:"+error);}
+    })
 }
+
+function completeGetExhibitions(results){
+	if (results["status"] != "success") {
+		// document.querySelector('#output').innerHTML = ErrQuery+": get sites";
+		return;
+	}
+	exhibitionList = results['exhibition'];
+	exhibitionLink = results['links'];
+	siteDropdown(exhibitionList, exhibitionLink);
+}
+
+// apply the event listener to all images
+document.querySelectorAll('.link').forEach(element => {
+	element.addEventListener('click', event => {
+		var word = document.getElementById('word-response').value;
+		var e = document.getElementById('slct');
+		exhibition = e.options[e.selectedIndex].text;
+		addWord(Exhibition, word);
+		
+		// change current displayed art
+	});
+});
+function completeAddWord(results){
+	var exhibition = results['exhibition'];
+	var emotion = results['word'];
+	console.log("Exhibition: "+exhibition+", Word: "+word);	
+	alert("Response recorded!")
+}
+
+function addWord(exhibition, word) {
+	fetch(baseUrl+'/response/word/'+exhibition+'/'+word, {
+        method: 'get'
+    })
+    .then (response => response.json() )
+    .then (data =>completeAddWord(data))
+    .catch(error => {
+        {alert("Error: Something went wrong:"+error);}
+    })
+}
+
+	// function getWord() {
+	//    word = document.getElementById('word-response').value;
+	//    fetch(baseUrl + '/response/word/'+word, {
+	//     method: 'get'
+	//    })
+	//    .then(response => response.json())
+	//    .then(data => completeGetWord(data))
+	//    .catch(error => {
+	//     {alert("Error: Something went wrong:" + error); }
+	//    })
+	// }
+	
+	
+	// function completeGetWord(results){
+	// 	var word = results['word'];
+	// 	console.log("Word: "+word);
+	// 	clearTextbox();
+	// 	alert("Success! Word added");
+	// }
 
 // Clears up the textbox once the word is sent
 function clearTextbox() {
