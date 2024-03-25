a1c9e4b6fa0c660392e86c63ab729726b68d48c5
Rose Winston <winston1@kenyon.edu>
2024-03-25 01:26:40 +0000

    word response + exhibition fully implemented and working!!!!

--- galleryFeedback.js
+++ galleryFeedback.js
@@ -11,7 +11,7 @@ var exhibitionList = [];
 var exhibitionLink = [];
 var word = ""; 
 
-	// document.getElementById('submit-btn').addEventListener("click", getWord); unsure if used for new code
+document.getElementById('submit-btn').addEventListener("click", addWord); 
 
 getAllExhibitions();
 
@@ -24,7 +24,6 @@ function siteDropdown(exhibitionList, exhibitionLink) {
 	})
 	
 	var imageList = "";
-	var count2 = 0;
 	exhibitionLink.forEach(function(key){
 		imageList += "<img id='"+key+"' src='"+gundUrl+key+"' alt='img' draggable='false'>";
 	})
@@ -56,24 +55,26 @@ function getAllExhibitions(){
 }
 
 // apply the event listener to all images
-document.querySelectorAll('.link').forEach(element => {
-	element.addEventListener('click', event => {
-		var word = document.getElementById('word-response').value;
-		var e = document.getElementById('slct');
-		exhibition = e.options[e.selectedIndex].text;
-		addWord(Exhibition, word);
+// document.querySelectorAll('.link').forEach(element => {
+// 	element.addEventListener('click', event => {
+// 		var word = document.getElementById('word-response').value;
+// 		var e = document.getElementById('slct');
+// 		var exhibition = e.options[e.selectedIndex].text;
+// 		console.log(exhibition);
+// 		addWord(exhibition, word);
 		
-		// change current displayed art
-	});
-});
-function completeAddWord(results){
-	var exhibition = results['exhibition'];
-	var emotion = results['word'];
-	console.log("Exhibition: "+exhibition+", Word: "+word);	
-	alert("Response recorded!")
-}
+// 		// change current displayed art
+// 	});
+// });
 
-function addWord(exhibition, word) {
+function addWord() {
+	var word = document.getElementById('word-response').value;
+	if (word=="") {
+		alert("Please enter a word");
+		return;
+	}
+	var e = document.getElementById('slct');
+	var exhibition = e.options[e.selectedIndex].text;
 	fetch(baseUrl+'/response/word/'+exhibition+'/'+word, {
         method: 'get'
     })
@@ -84,25 +85,33 @@ function addWord(exhibition, word) {
     })
 }
 
-	// function getWord() {
-	//    word = document.getElementById('word-response').value;
-	//    fetch(baseUrl + '/response/word/'+word, {
-	//     method: 'get'
-	//    })
-	//    .then(response => response.json())
-	//    .then(data => completeGetWord(data))
-	//    .catch(error => {
-	//     {alert("Error: Something went wrong:" + error); }
-	//    })
-	// }
-	
-	
-	// function completeGetWord(results){
-	// 	var word = results['word'];
-	// 	console.log("Word: "+word);
-	// 	clearTextbox();
-	// 	alert("Success! Word added");
-	// }
+function completeAddWord(results){
+	var exhibition = results['exhibition'];
+	var word = results['word'];
+	console.log("Exhibition: "+exhibition+", Word: "+word);	
+	alert("Success! Response recorded!")
+	clearTextbox();
+}
+
+// function getWord() {
+//    word = document.getElementById('word-response').value;
+//    fetch(baseUrl + '/response/word/'+word, {
+//     method: 'get'
+//    })
+//    .then(response => response.json())
+//    .then(data => completeGetWord(data))
+//    .catch(error => {
+//     {alert("Error: Something went wrong:" + error); }
+//    })
+// }
+
+
+// function completeGetWord(results){
+// 	var word = results['word'];
+// 	console.log("Word: "+word);
+// 	clearTextbox();
+// 	alert("Success! Word added");
+// }
 
 // Clears up the textbox once the word is sent
 function clearTextbox() {
