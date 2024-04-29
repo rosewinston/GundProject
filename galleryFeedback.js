aa585a59088e9d01e0b04e8da9b8784000a6f61c
Rose Winston <winston1@kenyon.edu>
2024-04-29 03:39:22 +0000

    major WIP for figuring out radio buttons, buttons autogenerated with bad formatting and do not turn back to light

--- galleryFeedback.js
+++ galleryFeedback.js
@@ -11,16 +11,57 @@ var exhibitionList = [];
 var exhibitionLink = [];
 var word = ""; 
 
-document.getElementById('submit-btn').addEventListener("click", addWord); 
+// document.getElementById('submit-btn').addEventListener("click", addWord); 
 
 getAllExhibitions();
 
-//Create the dropdown list for the site, containing each exhibition with an associated link 
+/* Code to operate radio buttons */
+// $('#radio-buttons label').click( function() {
+// 	$(this).addClass('btn-primary').removeClass('btn-light');
+// 	console.log("click button");
+// 	// var wz = document.getElementById('wlan_zone');
+// 	// console.log(wz.value,this.value)
+// 	// wz.value = this.value;
+// 	// console.log(wz.value,this.value)
+
+// 	// const buttons = document.getElementById("zone_buttons");
+// 	// for (const child of buttons.children) {
+// 	// 	if (child.value != this.value) {
+// 	// 		console.log("remove");
+// 	// $(child).addClass('btn-light').removeClass('btn-primary');
+// 	// 	}
+// 	// }
+// });
+
+function setupButtons(){
+    $('.radio-buttons label').click(function() {
+		$(this).addClass('btn-primary').removeClass('btn-light');
+        var id = $('#'+$(this).attr('for')).attr('id');
+   	 	var val = $('#'+$(this).attr('for')).val();
+		// const buttons = document.getElementById("exb_buttons");
+		// 	for (const child of buttons.children) {
+		// 		if (child.value != this.value) {
+		// 			console.log("remove");
+		// 	$(child).addClass('btn-light').removeClass('btn-primary');
+		// 		}
+		// 	}
+		updateSettings(id,val);
+		// console.log($(this));
+    });
+};
+
+function updateSettings(id,value){
+    console.log('clicked : ' +id+' ,Value: '+value);
+}
+
+//Create the radio buttons for the site, containing each exhibition with an associated link 
 function siteDropdown(exhibitionList, exhibitionLink) { 
-	var dropdown = "";
+	var buttons = "";
 	var countValue = 0;
+	var classInfo = "btn btn-light col-6 col-sm-4 btn";
 	exhibitionList.forEach(function(key){
-		dropdown += "<option id='"+countValue+"' value='"+exhibitionLink[countValue]+"'>"+key+"</option>";
+		buttons += '<input type="radio" class="btn-check" name="options" id="'+countValue+'" autocomplete="off">'
+		buttons += "<label class='"+classInfo+"' for='"+countValue+"'id='exb_buttons'>"+key+"</label>";
 		countValue += 1;
 	})
 	
@@ -29,8 +70,9 @@ function siteDropdown(exhibitionList, exhibitionLink) {
 		imageList += "<img id='"+key+"' src='"+key+"' alt='img' draggable='false'>";
 	})
 
-	document.querySelector('#slct').innerHTML = dropdown;
-	document.querySelector('.slider').innerHTML = imageList;
+	document.querySelector('#radio-buttons').innerHTML = buttons;
+	setupButtons();
+	// document.querySelector('.slider').innerHTML = imageList;
 	return;
 }
 
@@ -96,12 +138,12 @@ function selectExhibition(){
 	document.querySelector('label').innerHTML = label;
 }
 
-document.getElementById("slct").addEventListener("click",selectExhibition);
+// document.getElementById("slct").addEventListener("click",selectExhibition);
 
 // Code that changes the image on the slider when dropdown option is selected
-var dropdownList = document.getElementById('slct');
-dropdownList.addEventListener('change', event => {
-	currentLink = dropdownList.options[dropdownList.selectedIndex].value;
-	console.log(currentLink);
-	document.querySelector('.slider').innerHTML = "<img id='"+currentLink+"' src='"+currentLink+"' alt='img' draggable='false'>";
-});
+// var dropdownList = document.getElementById('slct');
+// dropdownList.addEventListener('change', event => {
+// 	currentLink = dropdownList.options[dropdownList.selectedIndex].value;
+// 	console.log(currentLink);
+// 	document.querySelector('.slider').innerHTML = "<img id='"+currentLink+"' src='"+currentLink+"' alt='img' draggable='false'>";
+// });
